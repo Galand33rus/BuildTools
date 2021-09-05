36dadc64f3e261ec6ca23c2652482e504a483d57
Andrew <galand_@mail.ru>
2021-09-05 19:04:12 +0300

    lesson2

---
+++ lesson2/js/main.js
@@ -0,0 +1,49 @@
+import { diffDates, diffToHtml } from "./datecalc.js";
+import { formatError } from "./utils.js";
+import { toggle } from "./toggle.js";
+import { timer } from "./timer.js";
+import "../css/main.scss";
+import '../media/notification.mp3';
+
+const log = console.log;
+const dateCalcForm = document.getElementById("datecalc");
+const dateCalcResult = document.getElementById("datecalc__result");
+const timerForm = document.getElementById("timer");
+const timerError = document.getElementById("error")
+//const media = require('../media/notification.mp3')
+
+dateCalcForm.addEventListener("submit", handleCalcDates);
+timerForm.addEventListener("submit", handleTimer);
+
+function handleCalcDates(event) {
+
+	event.preventDefault();
+	dateCalcResult.innerHTML = "";
+
+	let { firstDate, secondDate } = event.target.elements;
+	firstDate = firstDate.value;
+	secondDate = secondDate.value;
+
+	if (firstDate && secondDate) {
+		const diff = diffDates(firstDate, secondDate);
+		dateCalcResult.innerHTML = diffToHtml(diff);
+	} else dateCalcResult.innerHTML = formatError("Для расчета промежутка необходимо заполнить оба поля");
+}
+
+function handleTimer(event) {
+	event.preventDefault();
+
+	let timerStarting = event.target.elements.starting;
+	let timerStartingValue = timerStarting.value;
+	if (event.submitter.classList == "start-button" &&  timerStartingValue) {
+		timer(timerStartingValue);
+		timerStarting.value = "";
+		timerError.innerHTML = "";
+	} else if (!timerStartingValue || timerStartingValue == 0) {
+		timerError.innerHTML = formatError("Введите время");
+	} else {
+		timerStarting.value = "";
+	}
+}
+
+toggle();
